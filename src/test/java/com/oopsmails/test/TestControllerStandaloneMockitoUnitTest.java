4e91cd378aa9cf1be812b6243f9b94cfcf098e6e
oopsmails <oopsmails@gmail.com>
2017-07-12 21:47:02 -0400

    added Mockito spring bean unit test

--- src/test/java/com/oopsmails/test/TestControllerStandaloneMockitoUnitTest.java
+++ src/test/java/com/oopsmails/test/TestControllerStandaloneMockitoUnitTest.java
@@ -1,11 +1,14 @@
 package com.oopsmails.test;
 
+import com.oopsmails.model.User;
+import com.oopsmails.service.UserService;
+import com.oopsmails.test.config.WebConfigMockitoTest;
 import com.oopsmails.test.config.WebConfigTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,22 +18,36 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * Created by liu on 2017-07-07.
+ * Created by liu on 2017-07-12.
  */
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {WebConfigTest.class})
-public class TestControllerStandaloneUnitTest {
+@ContextConfiguration(classes = {WebConfigMockitoTest.class})
+public class TestControllerStandaloneMockitoUnitTest {
+    private static final AtomicInteger counter = new AtomicInteger();
+    private static List<User> users = new ArrayList<User>(
+            Arrays.asList(
+                    new User(counter.incrementAndGet(), "Daenerys Targaryen"),
+                    new User(counter.incrementAndGet(), "Cersei Baratheon")));
     @Autowired
     private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private UserService userService;
 
     private MockMvc mockMvc;
 
@@ -41,45 +58,36 @@ public class TestControllerStandaloneUnitTest {
 
     @Test
     public void test_get_all_success() throws Exception {
+        Mockito.when(userService.getAll()).thenReturn(users);
         mockMvc.perform(get("/users"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$", hasSize(4)))
-                .andExpect(jsonPath("$[0].id", is(1)))
-                .andExpect(jsonPath("$[0].username", is("Daenerys Targaryen")))
-                .andExpect(jsonPath("$[1].id", is(2)))
-                .andExpect(jsonPath("$[1].username", is("John Snow")));
-    }
-
-    // =========================================== Get Users by Ids ==========================================
-
-    @Test
-    public void test_get_by_ids_success() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/users").param("id", "1").param("id", "3"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].id", is(1)))
                 .andExpect(jsonPath("$[0].username", is("Daenerys Targaryen")))
-                .andExpect(jsonPath("$[1].id", is(3)))
-                .andExpect(jsonPath("$[1].username", is("Arya Stark")))
-                .andReturn();
-
-        String responseContent = mvcResult.getResponse().getContentAsString();
-        System.out.println("responseContent = " + responseContent);
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].username", is("Cersei Baratheon")));
     }
 
     @Test
-    public void test_get_by_ids_fail_01() throws Exception {
-        final String ERR_MSG = "Failed to convert value of type [java.lang.String[]] to required type [java.lang.Integer[]]";
-        MvcResult mvcResult = mockMvc.perform(get("/users").param("id", "a").param("id", "3"))
-                .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
+    public void test_get_by_ids_success() throws Exception {
+        Mockito.when(userService.getByIds(Mockito.anyList()))
+                .thenReturn(users);
+        MvcResult mvcResult = mockMvc.perform(
+                get("/users")
+                        .param("id", "1")
+                        .param("id", "3") //even here is 3, id = 2 is returned because of mockito
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].username", is("Daenerys Targaryen")))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].username", is("Cersei Baratheon")))
                 .andReturn();
 
         String responseContent = mvcResult.getResponse().getContentAsString();
         System.out.println("responseContent = " + responseContent);
-        String errMsg = mvcResult.getResolvedException().getMessage();
-        System.out.println("errMsg = " + errMsg);
-        assertEquals(true, errMsg.indexOf(ERR_MSG) >= 0);
     }
 }
